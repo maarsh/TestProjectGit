fab6467436dd2b2c8698c88bfbded8ad27718c28
Marseelan A <marseelan_a@infosys.com>
2016-06-11 13:51:46 +0530

    second commit

--- TestProject1/src/com/mars/HelloWorld.java
+++ TestProject1/src/com/mars/HelloWorld.java
@@ -5,6 +5,11 @@ public class HelloWorld {
 	public HelloWorld() {
 		System.out.println("Hello World");
 	}
+
+	public void test(){
+		System.out.println("inside test method ");
+		System.out.println("new file");
+	}
 	
 	public static void main(String[] args) {
 		new HelloWorld();
